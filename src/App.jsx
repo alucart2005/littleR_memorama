bdd8ccf8f1d099528a3b396539bab6064cd8d2d3
alucart2005 <napoleonanaya@gmail.com>
2023-12-09 17:22:57 -0500

    feature: working useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Tablero } from "./components/Tablero/Tablero";
+const emojiList = [..."💀👻🧛🌮🎱🍬🍕🦖"];
 
 function App() {
+  const [memobloquesbarajeados, setMemobloquesbarajeados] = useState([]);
+  useEffect(() => {
+    const barajadoEmojiLista = barajarArray([...emojiList, ...emojiList]);
+    setMemobloquesbarajeados(
+      barajadoEmojiLista.map((emoji, i) => ({
+        index: i,
+        emoji,
+        flipped: false,
+      }))
+    );
+  }, []);
+
+  const barajarArray = (a) => {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i],a[j]] = [a[j],a[i]];
+      console.log(a)
+    }
+    return a;
+  };
   return (
     <div>
-      <Tablero/>
+      <Tablero 
+      memoBlocks={memobloquesbarajeados}
+      />
     </div>
   );
 }
