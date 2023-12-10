ab1073155b08c27f1e287a8f7ff24f5649414e22
alucart2005 <napoleonanaya@gmail.com>
2023-12-09 20:02:53 -0500

    Feature: step 4

--- src/components/MemoBlock/MemoBlock.jsx
+++ src/components/MemoBlock/MemoBlock.jsx
@@ -1,16 +1,16 @@
-import "./MemoBlock.css"
+import "./MemoBlock.css";
 
-export function MemoBlock({memoBlock}) {
+export function MemoBlock({ memoBlock }) {
   return (
     <div className="memo-block">
-      <div>
-        <div className="memo-block-front">
-
-        </div>
-        <div className="memo-block-back">
-          {memoBlock.emoji}
-        </div>
+      <div
+        className={`memo-block-inner ${
+          memoBlock.flipped && "memo-block-flipped"
+        }`}
+      >
+        <div className="memo-block-front"></div>
+        <div className="memo-block-back">{memoBlock.emoji}</div>
       </div>
     </div>
-  )
+  );
 }
